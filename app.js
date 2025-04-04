e2f6aede331795f400e3c4f5af1330dbf1864098
rgautham7 <gauthamramesh98270@gmail.com>
2025-04-04 12:10:16 +0530

    Failed to lookup view deploy error resolve

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const blogRoutes = require('./routes/blogRoutes');
 
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3000;
 
 // EJS Setup
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
 app.use(express.static('public'));
